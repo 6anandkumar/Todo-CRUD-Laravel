1a2efe1ce5199434d807ceabf0dbecad4f4ab3d1
Anand Kumar <6anandkumar@gmail.com>
2020-10-05 16:13:35 +0530

    initialCommit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Todo;
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::all()->toArray();
+        return view('todo.index', compact('todos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('todo.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title'    =>  'required',
+            'desc'     =>  'required'
+        ]);
+        $todo = new Todo([
+            'title'    =>  $request->get('title'),
+            'desc'     =>  $request->get('desc')
+        ]);
+        $todo->save();
+        return redirect()->route('todo.index')->with('success', 'Data Added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $todo = Todo::find($id);
+        return view('todo.edit', compact('todo', 'id'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'title'    =>  'required',
+            'desc'     =>  'required'
+        ]);
+        $todo = Todo::find($id);
+        $todo->title = $request->get('title');
+        $todo->desc = $request->get('desc');
+        $todo->save();
+        return redirect()->route('todo.index')->with('success', 'Data Updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $todo = Todo::find($id);
+        $todo->delete();
+        return redirect()->route('todo.index')->with('success', 'Data Deleted');
+    }
+}
---
+++ resources/views/Todo/edit.blade.php
@@ -0,0 +1,35 @@
+@extends('master')
+
+@section('content')
+
+<div class="row">
+ <div class="col-md-12">
+  <br />
+  <h3>Edit Record</h3>
+  <br />
+  @if(count($errors) > 0)
+
+  <div class="alert alert-danger">
+         <ul>
+         @foreach($errors->all() as $error)
+          <li>{{$error}}</li>
+         @endforeach
+         </ul>
+  @endif
+  <form method="post" action="{{action('TodoController@update', $id)}}">
+   {{csrf_field()}}
+   <input type="hidden" name="_method" value="PATCH" />
+   <div class="form-group">
+    <input type="text" name="title" class="form-control" value="{{$todo->title}}" placeholder="Enter Title" />
+   </div>
+   <div class="form-group">
+    <input type="text" name="desc" class="form-control" value="{{$todo->desc}}" placeholder="Enter Description" />
+   </div>
+   <div class="form-group">
+    <input type="submit" class="btn btn-primary" value="Edit" />
+   </div>
+  </form>
+ </div>
+</div>
+
+@endsection
